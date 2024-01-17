ee338b4e53573645faaa30e26dc01aa5dbf52554
FornesBorja <120723832+FornesBorja@users.noreply.github.com>
2024-01-17 22:23:08 +0100

    opciones mejoras 1

--- app/src/main/java/com/example/mydaysapp/LoginActivity.java
+++ app/src/main/java/com/example/mydaysapp/LoginActivity.java
@@ -91,6 +91,7 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+
     @Override
     protected void onStart()
     {
--- app/src/main/java/com/example/mydaysapp/OpcionesActivity.java
+++ app/src/main/java/com/example/mydaysapp/OpcionesActivity.java
@@ -7,19 +7,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class OpcionesActivity extends AppCompatActivity {
-    Button cerrarSesion, cargarIMG;
+    Button cerrarSesion, cargarIMG, CorreoV, CorreoC;
     ImageView foto;
     private FirebaseStorage storage;
     private StorageReference storageReference;
@@ -27,16 +34,61 @@ public class OpcionesActivity extends AppCompatActivity {
     private Uri selectedImageUri = null; // Variable para almacenar la URI de la imagen seleccionada
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseUser currentUser = mAuth.getCurrentUser();
+    EditText correoTV;
+    String nuevoCorreo;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opciones);
-
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         cerrarSesion = findViewById(R.id.cerrarSesion_btn);
         foto = findViewById(R.id.imageView2);
         cargarIMG = findViewById(R.id.subirIMG_BTN);
+        correoTV= findViewById(R.id.mostrarCorreo);
+        CorreoV=findViewById(R.id.CorreoV);
+        CorreoC=findViewById(R.id.correoC);
+        CorreoC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               nuevoCorreo=correoTV.getText().toString();
+                if (esCorreoElectronicoValido(nuevoCorreo))
+                cambiarDireccionCorreo(nuevoCorreo);
+            }
+        });
+        CorreoV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                if (user != null) {
+                    user.sendEmailVerification()
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "Se ha enviado correo de verificación", Toast.LENGTH_SHORT);
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "No se puede enviar correo de verificación.", Toast.LENGTH_SHORT);
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
+        if (user != null && user.isEmailVerified()) {
+            // El usuario está verificado, deshabilita el botón
+            CorreoV.setEnabled(false);
+        } else {
+            // El usuario no está verificado, habilita el botón
+            CorreoV.setEnabled(true);
+        }
+        if (user != null) {
+            String correoUsuario = user.getEmail();
+            correoTV.setText(correoUsuario);
+        }
 
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
@@ -76,7 +128,25 @@ public class OpcionesActivity extends AppCompatActivity {
             }
         });
     }
+    private void cambiarDireccionCorreo(String nuevoCorreo) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
+        if (user != null) {
+            user.updateEmail(nuevoCorreo)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                // La dirección de correo electrónico ha sido actualizada exitosamente
+                                Toast.makeText(getApplicationContext(), "Correo electrónico actualizado", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // Manejar el error al actualizar la dirección de correo electrónico
+                                Toast.makeText(getApplicationContext(), "Error al actualizar el correo electrónico", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
     // Método para manejar el resultado de la selección de la imagen
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -109,5 +179,18 @@ public class OpcionesActivity extends AppCompatActivity {
                         Toast.makeText(this, "Error al subir la imagen: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                     });
         }
+    }
+    public static boolean esCorreoElectronicoValido(String correo) {
+        // Expresión regular para validar una dirección de correo electrónico
+        String expresionRegular = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$";
+
+        // Compilar la expresión regular en un patrón
+        Pattern pattern = Pattern.compile(expresionRegular);
+
+        // Crear un objeto Matcher
+        Matcher matcher = pattern.matcher(correo);
+
+        // Verificar si la dirección de correo electrónico coincide con la expresión regular
+        return matcher.matches();
     }
 }
